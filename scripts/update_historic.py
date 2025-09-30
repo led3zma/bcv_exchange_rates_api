45616d1f16949493568120f283d5a619852349ed
Alejandro Salas <asalasledezma16@gmail.com>
2025-09-30 12:15:03 -0400

    Extract get_value_date into reusable function for looping through excel file sheets

--- scripts/update_historic.py
+++ scripts/update_historic.py
@@ -2,30 +2,37 @@ import pandas as pd
 
 from datetime import datetime
 
+excel_path = "./input/historic/2_1_2c25_smc.xls"
 
-def main():
+
+def get_value_date(sheet_name: str):
     df = pd.read_excel(
-        "./input/historic/2_1_2c25_smc.xls",
+        excel_path,
         usecols="D",
         header=None,
         nrows=1,
         skiprows=4,
-        sheet_name="26092025",
+        sheet_name=sheet_name,
     )
     value_date = datetime.strptime(
         str(df.iloc[0, 0]).lstrip("Fecha Valor: "), "%d/%m/%Y"
     ).date()
-    print(value_date)
-    df = pd.read_excel(
-        "./input/historic/2_1_2c25_smc.xls",
+    return value_date
+
+
+def main():
+    excel = pd.read_excel(
+        excel_path,
         usecols="B,G",
         header=8,
         index_col=0,
         skiprows=[9],
         nrows=21,
-        sheet_name="26092025",
+        sheet_name=["26092025", "24092025"],
     )
-    print(df.loc["USD"].iloc[0])
+    for sheet_name, df in excel.items():
+        print(get_value_date(sheet_name=sheet_name))
+        print(df.loc["USD"].iloc[0])
 
 
 if __name__ == "__main__":
