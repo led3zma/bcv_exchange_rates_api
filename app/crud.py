fa7dbfaa56bdc97cc3755159c2b47b054bfc2e5d
Alejandro Salas <asalasledezma16@gmail.com>
2025-11-19 14:02:11 -0400

    Implement historical query endpoint

--- app/crud.py
+++ app/crud.py
@@ -4,11 +4,28 @@ from sqlalchemy.orm import Session
 from app.models import Rate
 
 
-def get_rate_by_date(session: Session, value_date: date) -> Rate | None:
+def _get_weekend_valid_date(value_date: date) -> date:
     # The saturday's and sunday's valid rate is the same for friday's rate
-    target_date = {
+    return {
         5: value_date - timedelta(days=1),
         6: value_date - timedelta(days=2),
     }.get(value_date.weekday(), value_date)
-    rate = session.scalars(select(Rate).where(Rate.value_date == target_date)).first()
+
+
+def get_rate_by_date(session: Session, value_date: date) -> Rate | None:
+    rate = session.scalars(
+        select(Rate).where(Rate.value_date == _get_weekend_valid_date(value_date))
+    ).first()
     return rate
+
+
+def get_rate_from_to_date(
+    session: Session, from_date: date, to_date: date
+) -> list[Rate] | None:
+    rates = session.scalars(
+        select(Rate).where(
+            Rate.value_date >= _get_weekend_valid_date(from_date),
+            Rate.value_date <= _get_weekend_valid_date(to_date),
+        )
+    )
+    return list(rates.all())
--- app/routers.py
+++ app/routers.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, status
 
 from app.core.db import SessionDep
 from app.models import RatePublic
-from app.crud import get_rate_by_date
+from app.crud import get_rate_by_date, get_rate_from_to_date
 
 rate_router = APIRouter(prefix="/rate")
 
@@ -18,3 +18,17 @@ async def get_rate(session: SessionDep, value_date: date | None = None) -> RateP
             status_code=status.HTTP_404_NOT_FOUND, detail="Rate not found"
         )
     return rate
+
+
+@rate_router.get("/historical", response_model=list[RatePublic])
+async def get_historical_rate(
+    session: SessionDep, from_date: date, to_date: date | None = None
+) -> list[RatePublic]:
+    if not to_date:
+        to_date = date.today()
+    rates = get_rate_from_to_date(session, from_date, to_date)
+    if not rates:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Empty search"
+        )
+    return rates
