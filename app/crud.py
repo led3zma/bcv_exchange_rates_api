2d4d9773f277e5e1903f281397c8de6fa2a21c08
Alejandro Salas <asalasledezma16@gmail.com>
2025-11-19 13:42:29 -0400

    Add validation for rate query if value date is saturday or sunday

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,14 @@
-from datetime import date
+from datetime import date, timedelta
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.models import Rate
 
 
 def get_rate_by_date(session: Session, value_date: date) -> Rate | None:
-    rate = session.scalars(select(Rate).where(Rate.value_date == value_date)).first()
+    # The saturday's and sunday's valid rate is the same for friday's rate
+    target_date = {
+        5: value_date - timedelta(days=1),
+        6: value_date - timedelta(days=2),
+    }.get(value_date.weekday(), value_date)
+    rate = session.scalars(select(Rate).where(Rate.value_date == target_date)).first()
     return rate
